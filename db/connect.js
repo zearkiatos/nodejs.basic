0ad8bfd99f850c761bdc42f45102d07aedc12600
Pedro Capriles <caprilespe@outlook.com>
2019-12-13 00:36:02 -0300

    Update docker configuration for mongodb

--- db/connect.js
+++ db/connect.js
@@ -1,21 +1,18 @@
 const MongoClient = require("mongodb").MongoClient;
 const { config } = require("./config");
-const connectionUrl = `mongodb://${config.DB_HOST}:${config.DB_PORT}`;
-console.log(connectionUrl);
-console.log(config.DB_HOST);
+const connectionUrl = `mongodb://root:root@${config.DB_HOST}:${config.DB_PORT}`;
 module.exports = (() => {
   let instance = null,
     isDisconnecting = false;
 
   function connect() {
       return new Promise((resolve, reject)=>{
-        MongoClient.connect(connectionUrl,{useNewUrlParser: true}, function(err,client){
+        MongoClient.connect(`${connectionUrl}`,{useNewUrlParser: true}, function(err,client){
             if(err) {
                 reject(err);
             }
             console.log("Conectado satisfactoriamente al servidor de Mongo!");
             instance = client;
-            console.log(client);
             resolve(client.db(config.DB_NAME));
         });
       })
